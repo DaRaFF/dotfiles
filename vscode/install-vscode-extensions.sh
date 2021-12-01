3d5bf00a6a0f13f1718c44d118ba05ada3e9bba8
Ralph Meier <ralphmeier79@gmail.com>
2021-12-01 14:42:59 +0100

    fix: vs code install extension fix

--- vscode/install-vscode-extensions.sh
+++ vscode/install-vscode-extensions.sh
@@ -1,59 +1,59 @@
 #!/usr/bin/env bash
 
 # arduino
-code --install-extension --force vsciot-vscode.vscode-arduino
+code --install-extension vsciot-vscode.vscode-arduino
 
 # eslint
-code --install-extension --force dbaeumer.vscode-eslint
+code --install-extension dbaeumer.vscode-eslint
 
 # github copilot AI
-code --install-extension --force GitHub.copilot
+code --install-extension GitHub.copilot
 
 # i8n plugin
-code --install-extension --force lokalise.i18n-ally
+code --install-extension lokalise.i18n-ally
 
 # git blame in status bar
-code --install-extension --force waderyan.gitblame
+code --install-extension waderyan.gitblame
 
 # kubernetes
-code --install-extension --force ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
 
 # real time collaboration
-code --install-extension --force ms-vsliveshare.vsliveshare
+code --install-extension ms-vsliveshare.vsliveshare
 
 # svg code completion support
-code --install-extension --force jock.svg
+code --install-extension jock.svg
 
 # markdown support (code completion / shortcuts)
-code --install-extension --force yzhang.markdown-all-in-one
+code --install-extension yzhang.markdown-all-in-one
 
 # prettify json
-code --install-extension --force mohsen1.prettify-json
+code --install-extension mohsen1.prettify-json
 
 # vue tooling
-code --install-extension --force octref.vetur
+code --install-extension octref.vetur
 
 # vs code file type icons
-code --install-extension --force vscode-icons-team.vscode-icons
+code --install-extension vscode-icons-team.vscode-icons
 
 # yaml support
-code --install-extension --force redhat.vscode-yaml
+code --install-extension redhat.vscode-yaml
 
 # theme - "one dark pro" (Atom‘s iconic One Dark theme for Visual Studio Code)
-code --install-extension --force zhuangtongfa.material-theme
+code --install-extension zhuangtongfa.material-theme
 
-# code --install-extension --force ms-azuretools.vscode-docker
-# code --install-extension --force GrapeCity.gc-excelviewer
-# code --install-extension --force felixfbecker.php-debug
-# code --install-extension --force felixfbecker.php-intellisense
-# code --install-extension --force felixfbecker.php-pack
-# code --install-extension --force GitHub.vscode-pull-request-github
-# code --install-extension --force ms-vscode-remote.remote-ssh
-# code --install-extension --force ms-vscode-remote.remote-ssh-edit
-# code --install-extension --force ms-vscode-remote.remote-wsl
-# code --install-extension --force ms-vscode-remote.vscode-remote-extensionpack
-# code --install-extension --force TabNine.tabnine-vscode
-# code --install-extension --force ms-vscode-remote.remote-containers
-# code --install-extension --force ms-vscode.cpptools
-# code --install-extension --force ms-vscode.sublime-keybindings
-# code --install-extension --force redhat.vscode-commons
+# code --install-extension ms-azuretools.vscode-docker
+# code --install-extension GrapeCity.gc-excelviewer
+# code --install-extension felixfbecker.php-debug
+# code --install-extension felixfbecker.php-intellisense
+# code --install-extension felixfbecker.php-pack
+# code --install-extension GitHub.vscode-pull-request-github
+# code --install-extension ms-vscode-remote.remote-ssh
+# code --install-extension ms-vscode-remote.remote-ssh-edit
+# code --install-extension ms-vscode-remote.remote-wsl
+# code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+# code --install-extension TabNine.tabnine-vscode
+# code --install-extension ms-vscode-remote.remote-containers
+# code --install-extension ms-vscode.cpptools
+# code --install-extension ms-vscode.sublime-keybindings
+# code --install-extension redhat.vscode-commons
